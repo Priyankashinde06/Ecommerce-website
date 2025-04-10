0f02fb8353e0aa400bc8a860bbc8d27a66c0e80f
Priyanka Shinde <140255605+Priyankashinde06@users.noreply.github.com>
2025-04-10 22:29:05 +0530

    Add files via upload

---
+++ src/components/GoogleMaps.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GoogleMaps = () => {
+    return (
+        <div className='map-area'>
+            <div className="maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387196.0767083608!2d-74.30915277037441!3d40.6966726870191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1742469878479!5m2!1sen!2sin"
+                ></iframe>
+            </div>
+        </div>
+    )
+}
+
+export default GoogleMaps
